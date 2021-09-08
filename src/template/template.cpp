85854fb0fc5186b424137537559e03bbd9815bc1
ZhenFei Xu <570635410@qq.com>
2021-09-08 10:58:49 +0800

    1

--- src/template/template.cpp
+++ src/template/template.cpp
@@ -10,7 +10,9 @@
 using namespace std;
 
 int main() {
-	
-	
+	int carrots;
+	cout << "How many carrots do you have?" << endl;
+	cin >> carrots;
+	cout << "You have " << carrots <<" carrots." << endl;
 	return 0;
 }
