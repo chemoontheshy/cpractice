66046034d28cb63833a816c29badd3856620b3db
ZhenFei Xu <570635410@qq.com>
2021-09-13 09:08:28 +0800

    1

--- src/practice/main.cpp
+++ src/practice/main.cpp
@@ -2,20 +2,14 @@
 
 using namespace std;
 
-struct RTSP_header
-{
-	unsigned char dollar_sign = 0x24;
-	unsigned char channel_identifier = 1;
-	unsigned short data_length = 1000;
-	//unsigned char stop_bit = 10;
-};
+
 
 int main() {
-	char test[11] = "0123456789";
-	cout << "test:" << +test[0] << endl;
-	cout << "test:" << &test << endl;
-	cout << "test:" << &test+1 << endl;
-	cout << "test" << +test << endl;
-	auto a = &test;
+	double m_n = 0.1548345;
+	cout << m_n << endl;
+	cout.precision(7);
+	cout << m_n << endl;
+
+	
 	return 0;
 }
--- src/template/template.cpp
+++ src/template/template.cpp
@@ -7,6 +7,8 @@
  * @Date: $time$
  */
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 struct Person
@@ -15,45 +17,75 @@ struct Person
 	int		age;
 };
 
-int main() {
-	//char carrots[20];
-	//cout << "How many carrots do you have?" << endl;
-	//cin.getline(carrots,20);
-	//cout << "You have " << carrots <<" carrots." << endl;
-	//int months[12] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
-	//cout << months[5] << endl;
-	//char 
-	//cout << UINT16_MAX << endl;
-	//Person zhangsan;
-	//zhangsan.age = 17;
-	//zhangsan.name = "张三";
-	//cout << zhangsan.name << "的年龄是" << zhangsan.age <<"岁。" << endl;
-	
-	//int tell[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	//cout << tell[9] << endl;
-	//cout << tell[0] << endl;
-	//cout << &tell[0] << endl;
-	//cout << &tell << endl;
+const int yInt(int& num) 
+{
+	num++;
+	return num;
+}
 
-	char animal[20] = "bear";
-	const char* bird = "wren";
-	char* ps;
-	cout << animal << " and ";
-	cout << bird << endl;
-	//cout <<ps<<"\n"
-	cout << animal << " at " << (int*)animal << endl;
-	cout << " Enter a kind of animal: ";
-	cin >> animal;
-	ps = animal;
-	cout << ps << "!\n";
-	cout << " Before using strcpy():\n";
-	cout << animal << " at " << (int*)animal << endl;
-	cout << ps << " at " << (int*)ps << endl;
-	ps = new char[strlen(animal) + 1];
-	strcpy_s(ps, strlen(animal) + 1, animal);
-	cout << "After using strcpy():\n";
-	cout << animal << " at " << (int*)animal << endl;
-	cout << ps << " at " << (int*)ps << endl;
-	delete[] ps;
+void file_it(ostream& os, double fo, const double fe[], int n);
+
+const int LIMIT = 5;
+
+int main() {
+	
+
+	ofstream fout;
+	const char* fn = "ep-data.txt";
+	fout.open(fn);
+	if (!fout.is_open())
+	{
+		cout << "Can't open " << fn << ". Bye." << endl;
+		exit(EXIT_FAILURE);
+	}
+	double objective;
+	cout << "Enter the focal length of your"
+		"telescope objective in mm: ";
+	cin >> objective;
+	double eps[LIMIT];
+	cout << "Enter the focal lengths, in mm of" << LIMIT << "eyepieces:\n";
+	for (int i = 0; i < LIMIT; i++)
+	{
+		cout << "Eyepice #" << i + 1 << ": ";
+		cin >> eps[i];
+	}
+	file_it(fout, objective, eps, LIMIT);
+	file_it(cout, objective, eps, LIMIT);
+	cout << "Done\n";
 	return 0;
+}
+
+void file_it(ostream& os, double fo, const double fe[], int n)
+{
+	// 标志格式
+	ios_base::fmtflags initial;
+	// 设置各种格式化状态
+	// ios_base::fixed 将对象置于使用定点表示法的模式
+	initial = os.setf(ios_base::fixed);
+	// 指定显示多少位小数
+	os.precision(0);
+	os << "Focal length of objective: " << fo << "mm\n";
+	// 将对象置于显示小数点的模式
+	os.setf(ios::showpoint);
+	// 指定显示多少位小数
+	os.precision(1);
+	// 设置下一次输出操作使用的字段宽度，默认0
+	os.width(12);
+	os << "f.l. eyspiece";
+	os.width(15);
+	os << "magnification" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		os.width(12);
+		os << fe[i];
+		os.width(15);
+		os << int(fo / fe[i] + 0.5) << endl;
+	}
+	os.setf(initial);
+	///LIFL
+	/*
+	*
+	* 
+	*/
+
 }
