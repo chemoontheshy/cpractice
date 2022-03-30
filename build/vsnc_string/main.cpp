137cf254ae2646c2617f722965b94c2a128a48b7
chemoontheshy <xzfandzgx@gmail.com>
2022-03-30 18:06:45 +0800

    增加智能指针的使用

--- build/vsnc_string/main.cpp
+++ build/vsnc_string/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+namespace vsnc
+{
+	namespace test
+	{
+		void __get_ip();
+
+		class Student
+		{
+		public:
+			Student(const std::string& name, const size_t age) :m_sName(name),m_iAge(age){}
+			void SelfIntroduction() noexcept { std::cout << "my name is " << m_sName <<", "<<m_iAge<< " years old"<<std::endl; }
+		private:
+			std::string m_sName;
+			size_t      m_iAge;
+		};
+	}
+}
+
+
 int main()
+{
+	std::unique_ptr<vsnc::test::Student> student;
+	student = std::make_unique<vsnc::test::Student>("Tom", 12);
+	student->SelfIntroduction();
+	std::cout << student.get() << std::endl;
+	student.reset();
+	student = std::make_unique<vsnc::test::Student>("chenmo", 25);
+	student->SelfIntroduction();
+	std::cout << student.get() << std::endl;
+
+	student.reset();
+	return 0;
+}
+
+void vsnc::test::__get_ip()
 {
 	auto __get_ip = [](const std::string& url, std::string& ip)->bool
 	{
@@ -27,7 +61,7 @@ int main()
 
 	std::string rtsp = "rtsp://192.168.31.62/11";
 	std::string ip;
-	if (!__get_ip(rtsp, ip)) return -1;
+	if (!__get_ip(rtsp, ip))
+		std::cout << "error" << std::endl;
 	std::cout << ip << std::endl;
-	return 0;
 }
